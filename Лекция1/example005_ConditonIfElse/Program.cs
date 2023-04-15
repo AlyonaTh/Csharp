83f371e01de150bf45266090206a14e533a7d8e0
Alyona <tyoaa51@gmail.com>
2023-04-15 13:27:06 +0600

    add Example 005 Condition If-Else

---
+++ Лекция1/example005_ConditonIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() =="masha")
+{
+    Console.WriteLine("Uhuu, this is MASHA!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
